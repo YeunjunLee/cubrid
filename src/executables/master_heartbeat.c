bc6a498972258f10808037bd59ecc8ba2beb0290
yjlee <yjlee@cubrid.com>
2025-01-06 12:37:56 +0900

    Change message type

--- src/executables/master_heartbeat.c
+++ src/executables/master_heartbeat.c
@@ -3599,7 +3599,9 @@ hb_resource_job_confirm_start (HB_JOB_ARG * arg)
 	  /* shutdown working server processes to change its role to slave */
 	  snprintf (hb_info_str, HB_INFO_STR_MAX, "%s The master node failed to restart the server process",
 		    HA_FAILBACK_DIAG_STRING);
-	  MASTER_ER_SET (ER_ERROR_SEVERITY, ARG_FILE_LINE, ER_HB_NODE_EVENT, 1, hb_info_str);
+	  snprintf (error_string, LINE_MAX, "(args:%s)", proc->args);
+
+	  MASTER_ER_SET (ER_ERROR_SEVERITY, ARG_FILE_LINE, ER_HB_PROCESS_EVENT, 2, hb_info_str, error_string);
 	  error = hb_resource_job_queue (HB_RJOB_DEMOTE_START_SHUTDOWN, NULL, HB_JOB_TIMER_IMMEDIATELY);
 	  assert (error == NO_ERROR);
 
